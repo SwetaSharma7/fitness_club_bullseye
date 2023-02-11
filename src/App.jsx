a83463f71b734916d5887cbbbe36820d98ba3f3e
Sweta <swetasharma.naju99@gmail.com>
2023-02-11 15:31:58 +0530

    adding 2nd button

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import "./app.css";
+import FormInput from "./components/FormInput";
+
+const App = () => {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "Name",
+      type: "text",
+      placeholder: "Name",
+      errorMessage:
+        "Username should be 3-16 characters and shouldn't include any special character!",
+      label: "Username",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "It should be a valid email address!",
+      label: "Email",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "birthday",
+      type: "date",
+      placeholder: "Birthday",
+      label: "Birthday",
+    },
+    {
+      id: 4,
+      name: "Phone Number",
+      type: "text",
+      placeholder: "Number",
+      label: "Phone Number",
+      required: true,
+    },
+
+    {
+      id: 4,
+      name: "address",
+      type: "text",
+      placeholder: "Address",
+      label: "Address",
+      required: true,
+    },
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  return (
+    
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <h1>Fitness Club Registration</h1>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <button>Add member</button>
+        <button>View Member List</button>
+      </form>
+      
+    </div>
+  );
+};
+
+export default App;
